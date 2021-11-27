c7e35ca2a832ab875877eb3c38a02101f7adc3a2
Netwid <me@netwid.dev>
2021-11-27 22:57:41 +0300

    RSA support

--- P2P/src/main/resources/tulahack/src/utils/rsaGenerator.ts
+++
@@ -1,9 +0,0 @@
-
-type keyPair = {
-    public: string,
-    private: string
-}
-
-export const generateRSA = (seed: string): keyPair => {
-    return {public: 'a', private: 'b' };
-}
---
+++ P2P/src/main/resources/tulahack/src/utils/rsaModule.ts
@@ -0,0 +1,32 @@
+import rsa from 'js-crypto-rsa'
+
+type KeyPair = {
+    pubKey: JsonWebKey,
+    privKey: JsonWebKey
+}
+
+export const generateRSA = async (seed: string): Promise<KeyPair> => {
+    return rsa.generateKey(2048).then( (key) => {
+        return {pubKey: key.publicKey, privKey: key.privateKey};
+    });
+}
+
+export const encryptMsg = async (plainMsg: string, pubKey: JsonWebKey): Promise<Uint8Array> => {
+    return rsa.encrypt(
+        new TextEncoder().encode(plainMsg),
+        pubKey,
+        'SHA-256',
+    ).then((encrypted) => {
+        return encrypted;
+    });
+}
+
+export const decryptMsg = async (encryptedMsg: Uint8Array, privKey: JsonWebKey): Promise<string> => {
+    return rsa.decrypt(
+      encryptedMsg,
+      privKey,
+      'SHA-256',
+    ).then((decrypted) => {
+        return new TextDecoder().decode(decrypted);
+    });
+}
