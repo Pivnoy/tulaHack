06cdced18128dc1c89fde6e747db52752377f60b
Netwid <me@netwid.dev>
2021-11-28 04:02:23 +0300

    Upd REST

--- P2P/src/main/java/com/example/p2p/controller/MessageController.java
+++ P2P/src/main/java/com/example/p2p/controller/MessageController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
 public class MessageController {
-
-
     private SimpMessagingTemplate messagingTemplate;
 
     @Autowired
--- P2P/src/main/java/com/example/p2p/controller/RegistrationController.java
+++ P2P/src/main/java/com/example/p2p/controller/RegistrationController.java
@@ -1,6 +1,7 @@
 package com.example.p2p.controller;
 
 import com.example.p2p.service.UserService;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,20 +18,27 @@ public class RegistrationController {
     @GetMapping("/registration")
     @CrossOrigin
     @ResponseBody
-    public ResponseEntity<Boolean> registration(@RequestParam(name = "login") String login,
-                                                  @RequestParam(name = "password") String password){
-        System.out.println("reg: " + login +" "+ password);
-        if (userService.existsByLogin(login) || password.length()<8)
-            return ResponseEntity.ok(Boolean.FALSE);
+    public ResponseEntity<String> registration(@RequestParam(name = "login") String login,
+                                               @RequestParam(name = "password") String password){
+        System.out.println("reg: " + login + " " + password);
+
+        if (userService.existsByLogin(login))
+            return ResponseEntity.ok((new JSONObject()).put("status", "fail").put("message", "Login already exists").toString());
+        if (password.length() < 8)
+            return ResponseEntity.ok((new JSONObject()).put("status", "fail").put("message", "Password is less than 8 characters").toString());
+
         userService.addNewUser(login, String.valueOf(password.hashCode()));
-        return ResponseEntity.ok(Boolean.TRUE);
+        return ResponseEntity.ok((new JSONObject()).put("status", "ok").toString());
     }
 
     @GetMapping("/check_login")
     @CrossOrigin
     @ResponseBody
-    public ResponseEntity<Boolean> checkLogin(@RequestParam(name = "login") String login) {
+    public ResponseEntity<String> checkLogin(@RequestParam(name = "login") String login) {
         System.out.println("check log: " + login);
-        return ResponseEntity.ok(userService.existsByLogin(login));
+
+        if (userService.existsByLogin(login))
+            return ResponseEntity.ok((new JSONObject()).put("status", "fail").put("message", "Login already exists").toString());
+        return ResponseEntity.ok((new JSONObject()).put("status", "ok").toString());
     }
 }
